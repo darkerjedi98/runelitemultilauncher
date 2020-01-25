01de349a56f34bf3d169147f149d3be4993238dd
darkerjedi98 <43352485+darkerjedi98@users.noreply.github.com>
2020-01-25 05:00:08 -0500

    Add files via upload

--- Launcher.py
+++ Launcher.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 import os
 import keyboard
+import webbrowser
 from tkinter import *
+from tkinter import simpledialog
+
 
 def open():
     os.startfile(r'notepad.exe')
@@ -14,7 +17,18 @@ def open4():
 def open5():
     os.startfile(r'RuneLite.exe')
     os.startfile(r'RuneLite.exe')
-
+def launchcode():
+    coin = (int(input('coins spent:')))
+    rune = (int(input('nature rune cost:')))
+    item = (int(input('# of items bought:')))
+    price = (int(input('price per item:')))
+    back = (int(input('money back(after selling):')))
+    print(rune * item - coin - price + back)
+    (print("profit:"))
+def browser1():
+    webbrowser.open_new_tab('www.google.ca')
+def browser2():
+    webbrowser.open_new_tab('https://www.ge-tracker.com/osrs-market-watch')
 HEIGHT = 220
 WIDTH = 270
 
@@ -27,10 +41,12 @@ background_image = tk.PhotoImage(file='image.png')
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
-
 button = tk.Button(root, text="classic multi launcher", bg='green', fg='black', height=3, width=17, command=open2)
 
+
+
 button.pack(side='left')
+
 
 button = tk.Button(root, text="Runelite", bg='green', fg='black', height=3, width=17, command=open4)
 
@@ -47,6 +63,16 @@ button.pack(side='right')
 
 button = tk.Button(root, text="launch 2 runelite's", bg='green', fg='black', height=3, width=17, command=open5)
 
+button.pack(side='left')
+
+button = tk.Button(root, text="profit calc", bg='green', fg='black', height=3, width=17, command=launchcode)
+
+button.pack(side='left')
+
+button = tk.Button(root, text="google", bg='green', fg='black', height=3, width=17, command=browser1)
+button.pack(side='left')
+
+button = tk.Button(root, text="ge tracker", bg='green', fg='black', height=3, width=17, command=browser2)
 button.pack(side='left')
 
 root.mainloop()
