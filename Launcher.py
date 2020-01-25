e1c461321effda3b9ea55921b6d6b3a2f3802573
darkerjedi98 <43352485+darkerjedi98@users.noreply.github.com>
2020-01-24 21:39:48 -0500

    Add files via upload

---
+++ Launcher.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+import os
+import keyboard
+from tkinter import *
+
+def open():
+    os.startfile(r'notepad.exe')
+def open2():
+    os.startfile(r'code1.py')
+def open3():
+    os.startfile(r'calc')
+def open4():
+    os.startfile(r'RuneLite.exe')
+
+HEIGHT = 220
+WIDTH = 270
+
+root = tk.Tk()
+root.title("Darkerjedi's Launcher")
+canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH,)
+canvas.pack()
+
+background_image = tk.PhotoImage(file='image.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+
+button = tk.Button(root, text="classic multi launcher", bg='green', fg='black', height=3, width=17, command=open2)
+
+button.pack(side='left')
+
+button = tk.Button(root, text="Runelite", bg='green', fg='black', height=3, width=17, command=open4)
+
+button.pack(side='left')
+
+
+button = tk.Button(root, text="notepad", bg='green', fg='black', height=3, width=7, command=open)
+
+button.pack(side='right')
+
+button = tk.Button(root, text="calc", bg='green', fg='black', height=3, width=5, command=open3)
+
+button.pack(side='right')
+
+root.mainloop()
---
+++ code1.py
@@ -0,0 +1,130 @@
+print("Runelite MultiLauncher By DarkerJedi98")
+import base64
+import os
+print("epic!")
+print("place this file in the same location as RuneLite.exe!")
+print("This will launch Oldschool Runescape Via Runelite! *works on windows 10 / 8 / 7 ")
+input("Press Enter to continue...")
+print("how many instances would you like to launch ? up to 4")
+print(" x=1 z=2 y=3 a=4 b=0 ")
+option1 = input("enter selection:")
+if option1 == "x":
+	os.startfile(r'Runelite.exe')
+	print("thank you for using darkerjedi's runescape launcher!")
+	input("Press Enter to continue...")
+	print("would you like to launch calculator and notepad?")
+	print(" a=launch both b=launch only calc c=launch only notepad n=launch none ")
+	option3 = input("enter selection:")
+	if option3 == "a":
+		print("launching notepad and calculator")
+		os.startfile(r'notepad.exe')
+		os.startfile(r'calc')
+		input("Press Enter to continue...")
+	if option3 == "b":
+		print("launching calculator")
+		os.startfile(r'calc')
+		input("Press Enter to continue...")
+	if option3 == "c":
+		print("launching notepad")
+		os.startfile(r'notepad.exe')
+		input("Press Enter to continue...")
+	if option3 == "n":
+		print("Thank you for Using Darkerjedi's Runelite Launcher!")
+		input("Press Enter to continue...")
+if option1 == "z":
+	os.startfile(r'Runelite.exe')
+	os.startfile(r'Runelite.exe')
+	print("thank you for using darkerjedi's runescape launcher!")
+	input("Press Enter to continue...")
+	print("would you like to launch calculator and notepad?")
+	print(" a=launch both b=launch only calc c=launch only notepad n=launch none")
+	option3 = input("enter selection:")
+	if option3 == "a":
+		print("launching notepad and calculator")
+		os.startfile(r'notepad.exe')
+		os.startfile(r'calc')
+		input("Press Enter to continue...")
+	if option3 == "b":
+		print("launching calculator")
+		os.startfile(r'calc')
+		input("Press Enter to continue...")
+	if option3 == "c":
+		print("launching notepad")
+		os.startfile(r'notepad.exe')
+		input("Press Enter to continue...")
+	if option3 == "n":
+		print("Thank you for Using Darkerjedi's Runelite Launcher!")
+		input("Press Enter to continue...")
+if option1 == "y":
+	os.startfile(r'Runelite.exe')
+	os.startfile(r'Runelite.exe')
+	os.startfile(r'Runelite.exe')
+	print("thank you for using darkerjedi's runescape launcher!")
+	input("Press Enter to continue...")
+	print("would you like to launch calculator and notepad?")
+	print(" a=launch both b=launch only calc c=launch only notepad n=launch none ")
+	option3 = input("enter selection:")
+	if option3 == "a":
+		print("launching notepad and calculator")
+		os.startfile(r'notepad.exe')
+		os.startfile(r'calc')
+		input("Press Enter to continue...")
+	if option3 == "b":
+		print("launching calculator")
+		os.startfile(r'calc')
+		input("Press Enter to continue...")
+	if option3 == "c":
+		print("launching notepad")
+		os.startfile(r'notepad.exe')
+		input("Press Enter to continue...")
+	if option3 == "n":
+		print("Thank you for Using Darkerjedi's Runelite Launcher!")
+		input("Press Enter to continue...")
+if option1 == "a":
+	os.startfile(r'Runelite.exe')
+	os.startfile(r'Runelite.exe')
+	os.startfile(r'Runelite.exe')
+	os.startfile(r'Runelite.exe')
+	print("thank you for using darkerjedi's runescape launcher!")
+	input("Press Enter to continue...")
+	print("would you like to launch calculator and notepad?")
+	print(" a=launch both b=launch only calc c=launch only notepad n=launch none")
+	option3 = input("enter selection:")
+	if option3 == "a":
+		print("launching notepad and calculator")
+		os.startfile(r'notepad.exe')
+		os.startfile(r'calc')
+		input("Press Enter to continue...")
+	if option3 == "b":
+		print("launching calculator")
+		os.startfile(r'calc')
+		input("Press Enter to continue...")
+	if option3 == "c":
+		print("launching notepad")
+		os.startfile(r'notepad.exe')
+		input("Press Enter to continue...")
+	if option3 == "n":
+			print("Thank you for Using Darkerjedi's Runelite Launcher!")
+			input("Press Enter to continue...")
+if option1 == "b":
+	print("thank you for using darkerjedi's runescape launcher!")
+	input("Press Enter to continue...")
+	print("would you like to launch calculator and notepad?")
+	print(" a=launch both b=launch only calc c=launch only notepad n=launch none n=launch none")
+	option3 = input("enter selection:")
+	if option3 == "a":
+		print("launching notepad and calculator")
+		os.startfile(r'notepad.exe')
+		os.startfile(r'calc')
+		input("Press Enter to continue...")
+	if option3 == "b":
+		print("launching calculator")
+		os.startfile(r'calc')
+		input("Press Enter to continue...")
+	if option3 == "c":
+		print("launching notepad")
+		os.startfile(r'notepad.exe')
+		input("Press Enter to continue...")
+	if option3 == "n":
+		print("Thank you for Using Darkerjedi's Runelite Launcher!")
+		input("Press Enter to continue...")
