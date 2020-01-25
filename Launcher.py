a64f2e83b480ad68270738ab54d7d2eddd87d694
darkerjedi98 <43352485+darkerjedi98@users.noreply.github.com>
2020-01-25 10:04:28 -0500

    Add files via upload

--- Launcher.py
+++ Launcher.py
@@ -4,6 +4,7 @@ import keyboard
 import webbrowser
 from tkinter import *
 from tkinter import simpledialog
+from tkinter.ttk import *
 
 
 def open():
@@ -26,9 +27,22 @@ def launchcode():
     print(rune * item - coin - price + back)
     (print("profit:"))
 def browser1():
-    webbrowser.open_new_tab('www.google.ca')
+    webbrowser.get('windows-default').open('www.google.ca')
 def browser2():
-    webbrowser.open_new_tab('https://www.ge-tracker.com/osrs-market-watch')
+    webbrowser.get('windows-default').open('https://www.ge-tracker.com/osrs-market-watch')
+def getweather():
+    city = "CAON4756:1:CA"
+    webbrowser.get('windows-default').open('https://weather.com/en-CA/weather/today/l/'+city)
+def duckduckgoose():
+    webbrowser.get('windows-default').open('http://duckduckgo.com')
+def facebook():
+    webbrowser.get('windows-default').open('http://facebook.com')
+def messenger():
+    webbrowser.get('windows-default').open('https://www.facebook.com/messages/t/')
+def youtube():
+    webbrowser.get('windows-default').open('https://www.youtube.com')
+
+
 HEIGHT = 220
 WIDTH = 270
 
@@ -44,35 +58,49 @@ background_label.place(relwidth=1, relheight=1)
 button = tk.Button(root, text="classic multi launcher", bg='green', fg='black', height=3, width=17, command=open2)
 
 
-
-button.pack(side='left')
+button.pack(fill = BOTH, expand = True)
 
 
 button = tk.Button(root, text="Runelite", bg='green', fg='black', height=3, width=17, command=open4)
 
-button.pack(side='left')
+button.pack(fill = BOTH, expand = True)
 
 
 button = tk.Button(root, text="notepad", bg='green', fg='black', height=3, width=7, command=open)
 
-button.pack(side='right')
+button.pack(fill = BOTH, expand = True)
 
 button = tk.Button(root, text="calc", bg='green', fg='black', height=3, width=5, command=open3)
 
-button.pack(side='right')
+button.pack(fill = BOTH, expand = True)
 
 button = tk.Button(root, text="launch 2 runelite's", bg='green', fg='black', height=3, width=17, command=open5)
 
-button.pack(side='left')
+button.pack(fill = BOTH, expand = True)
 
 button = tk.Button(root, text="profit calc", bg='green', fg='black', height=3, width=17, command=launchcode)
 
-button.pack(side='left')
+button.pack(fill = BOTH, expand = True)
 
 button = tk.Button(root, text="google", bg='green', fg='black', height=3, width=17, command=browser1)
-button.pack(side='left')
+button.pack(fill = BOTH, expand = True)
 
 button = tk.Button(root, text="ge tracker", bg='green', fg='black', height=3, width=17, command=browser2)
+button.pack(side='left')
+
+button = tk.Button(root, text="get weather", bg='green', fg='black', height=3, width=17, command=getweather)
+button.pack(side='left')
+
+button = tk.Button(root, text="duckduckgo", bg='green', fg='black', height=3, width=17, command=duckduckgoose)
+button.pack(side='left')
+
+button = tk.Button(root, text="facebook", bg='green', fg='black', height=3, width=17, command=facebook)
+button.pack(side='left')
+
+button = tk.Button(root, text="messenger", bg='green', fg='black', height=3, width=17, command=messenger)
+button.pack(side='left')
+
+button = tk.Button(root, text="youtube", bg='green', fg='black', height=3, width=17, command=youtube)
 button.pack(side='left')
 
 root.mainloop()
